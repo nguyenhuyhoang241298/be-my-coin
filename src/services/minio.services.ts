553c90a1b5dd4747d17cf3ca5d588fdcd92364d4
nguyenhuyhoang241298 <nguyenhuyhoang241298@gmail.com>
2025-12-22 20:59:16 +0700

    feat(upload-file): fix ssl

--- src/services/minio.services.ts
+++ src/services/minio.services.ts
@@ -1,16 +1,14 @@
 import * as minio from 'minio'
-import { replaceMinioUrl } from './helpers'
 
 const minioClient = new minio.Client({
   endPoint: process.env.MINIO_ENDPOINT!,
-  port: Number(process.env.MINIO_PORT),
-  useSSL: false,
+  port: 443,
+  useSSL: true,
   accessKey: process.env.MINIO_ACCESS,
   secretKey: process.env.MINIO_SECRET
 })
 
 export const getPresignedPutUrlService = async (bucketName: string, objectName: string, expiry?: number) => {
-  const url = await minioClient.presignedPutObject(bucketName, objectName, expiry)
-  return replaceMinioUrl(url)
+  return await minioClient.presignedPutObject(bucketName, objectName, expiry)
 }
 export default minioClient
